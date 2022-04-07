a1a37774a5dd6e03c993b988600eaba4c83fe15d
Snehita Kolli <snehitakolli20@gmail.com>
2022-04-06 23:41:19 -0700

    added moment js for current date to jsfile

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,16 @@
-// Open planner
+// Open planner--> open the website
     // Current day is displayed at top of the page--> use moment.js
-
+    $("#currentDay").text(moment().format('dddd, MMM Do'));
 // Scroll down calendar
-    // Timeblocks are present for standard business hours (9-5pm)
+    // Timeblocks are present for standard business hours (9-5pm)-->within the HTML file
 
 // Timeblocks features 
     // Color-coded: past events (grey), present events(red), future events(green)
+  
 
 // For EACH timeblock: 
     // Can enter the event + SAVE event in the timeblock --> use localStorage
+    
 
 // Refresh page--> saved event is still there 
+
