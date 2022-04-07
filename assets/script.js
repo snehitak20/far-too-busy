e1fbc461d73018b5eb36be1d113772d7936a7c2e
Snehita Kolli <snehitakolli20@gmail.com>
2022-04-06 18:44:12 -0700

    pseudocode finished in js file

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+// Open planner
+    // Current day is displayed at top of the page--> use moment.js
+
+// Scroll down calendar
+    // Timeblocks are present for standard business hours (9-5pm)
+
+// Timeblocks features 
+    // Color-coded: past events (grey), present events(red), future events(green)
+
+// For EACH timeblock: 
+    // Can enter the event + SAVE event in the timeblock --> use localStorage
+
+// Refresh page--> saved event is still there 
