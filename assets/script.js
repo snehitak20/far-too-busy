3a0871dd82cd6f5743b17a3f89e7eb5235954608
Snehita Kolli <snehitakolli20@gmail.com>
2022-04-07 18:11:45 -0700

    fixed colors for timeblocks and fixed timeblock ids

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,32 @@
 // Open planner--> open the website
     // Current day is displayed at top of the page--> use moment.js
-    $("#currentDay").text(moment().format('dddd, MMM Do YYYY'));
+    var day = moment().format('dddd, MMM Do YYYY');
+    $("#currentDay").html(day);
 // Scroll down calendar
     // Timeblocks are present for standard business hours (9-5pm)-->within the HTML file (DONE)
+    function colorBlock() {
+        var hourNow = moment().hour();
 
-// Timeblocks features 
-    // Color-coded: past events (grey), present events(red), future events(green)
-  
+        $(".time-block").each(function() {
+            var hourTime = parseInt($(this).attr("id").split('-')[1]);
 
+            if(hourTime < hourNow) {
+                $(this).addClass("past");
+            }
+            else if(hourTime === hourNow) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        });
+    }
+
+    colorBlock();
+    var interval = setInterval(colorBlock, 20000)
 // For EACH timeblock: 
     // Can enter the event + SAVE event in the timeblock --> use localStorage
     $(".saveBtn").on("click", function(){
@@ -16,16 +35,14 @@
 
         localStorage.setItem(time, info);
     })
-
-// Refresh page--> saved event is still there 
-    $("#9 .description").val(localStorage.getItem("9"));
-    $("#10 .description").val(localStorage.getItem("10"));
-    $("#11 .description").val(localStorage.getItem("11"));
-    $("#12 .description").val(localStorage.getItem("12"));
-    $("#1 .description").val(localStorage.getItem("1"));
-    $("#2 .description").val(localStorage.getItem("2"));
-    $("#3 .description").val(localStorage.getItem("3"));
-    $("#4 .description").val(localStorage.getItem("4"));
-    $("#5 .description").val(localStorage.getItem("5"));
-
-    
+        
+     // Refresh page--> saved event is still there 
+        $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+        $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+        $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+        $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+        $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+        $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+        $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+        $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+        $("#hour-17 .description").val(localStorage.getItem("hour-17"));
